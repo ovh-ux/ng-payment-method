5e98f6bfb601400c063844ed4ef63096440fae48
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2019-08-12 10:58:02 +0200

    fix: typo and callback param

--- src/components/register/controller.js
+++ src/components/register/controller.js
@@ -5,7 +5,7 @@ export default class OvhPaymentMethodRegisterCtrl {
     // if it's a function reference ...
     // otherwise the call will be made passing an Object Literal
     // when testing if the callback function is a function ref or not
-    if (isFunction(this.onInit(componentInstance))) {
+    if (isFunction(this.onInit({ componentInstance }))) {
       // ... invoke it
       this.onInit()(componentInstance);
     }
